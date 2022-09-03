e83f70c412e47d0b0e5e8c8ca77dd174ddcfef91
TOM-BROWN <tombrownanuma@gmail.com>
2022-09-03 20:02:20 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from "./components/NavBar";
+import Start from "./components/Start_page";
+import QuestionPage from "./components/Question_page";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import React, {useEffect} from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default function App() {
+    const [classNameCorrect, setClassNameCorrect] = React.useState("active")
+    const [classNameWrong, setClassNameWrong] = React.useState("active")
+    const [defaultState, setDefaultState] = React.useState("question-button")
+    const [checkAnswer, setCheckAnswer] = React.useState()
+
+
+    const [TriviaApiData, setTriviaApiData] = React.useState([])
+
+    useEffect(() => {
+        axios.get("http://localhost:3000/results")
+            .then(res => {
+                setTriviaApiData(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }, [])
+
+
+    const QuestionPageElement = TriviaApiData.map((data, index) => {
+        return (
+            <QuestionPage
+                key={index}
+                data={data}
+
+                defaultState={defaultState}
+                classNameCorrect={classNameCorrect}
+                classNameWrong={classNameWrong}
+                setCheckAnswer={setCheckAnswer}
+                handleClicked={handleClicked}
+            />
+        )
+    })
+
+    function handleClicked() {
+
+        setClassNameCorrect("correct")
+        setClassNameWrong("wrong")
+
+        console.log(classNameCorrect)
+        console.log(classNameWrong)
+
+    }
+
+
+    return (
+        <Router>
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Start/>}/>
+                <Route path="/question" element={
+                    <div>
+                        {QuestionPageElement}
+                        <div className="check-answer-container">
+                            <button
+                                onClick={handleClicked}
+                                className="check-answer">Check answer
+                            </button>
+                        </div>
+                        <hr/>
+                    </div>
+
+                }/>}/>
+            </Routes>
+        </Router>
+    );
 }
 
-export default App;
+
